c0a9fb41a7d6beb3a90fe172b525148665751f28
mindaugasI <mindaugas.ivanauskas@gmail.com>
2024-09-26 16:45:06 +0300

    ADD: game_stats.py for future statistis & calculation of left ships. Modified: alien_invasion.py, settings.py & ahip.py for evaluation of ships left & end of the game.

--- settings.py
+++ settings.py
@@ -9,10 +9,11 @@ class Settings:
 
         # Ship settings
         self.ship_speed = 1.5
+        self.ship_limit = 3
 
         # Bullet settings
         self.bullet_speed = 1.5
-        self.bullet_width = 300
+        self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
