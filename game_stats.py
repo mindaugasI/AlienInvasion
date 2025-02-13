61ce879bcae0f44696a1bcea5c6d19a113f44f39
mindaugasI <mindaugas.ivanauskas@gmail.com>
2025-02-13 15:00:09 +0200

    Add button.py to start the game and modified game_stats.py

--- game_stats.py
+++ game_stats.py
@@ -4,8 +4,9 @@ class GameStats:
         """Initialize statistics."""
         self.settings = ai_game.settings
         self.reset_stats()
-        # Start Alien Invasion in an active state.
-        self.game_active = True
+
+        # Start Alien Invasion in an inactive state.
+        self.game_active = False
 
     def reset_stats(self):
         """Initialize statistics that can change during the game."""
